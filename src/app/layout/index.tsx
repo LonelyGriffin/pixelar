ff3fdbdd87900c2e21447efc05c3c723365c99b4
LonelyGriffin <lonelygriffin@gmail.com>
2018-12-28 18:42:20 +0300

    add borders for layout

--- src/app/layout/index.tsx
+++ src/app/layout/index.tsx
@@ -12,13 +12,13 @@ export class Layout extends Component {
                 </div>
                 <div className={style.Layout_Content}>
                     <div className={style.Layout_Content_Left}>
-1
+
                     </div>
                     <div className={style.Layout_Content_Center}>
-2
+
                     </div>
                     <div className={style.Layout_Content_Right}>
-3
+
                     </div>
                 </div>
                 <div className={style.Layout_StatusBar}>
